810fab66003253c356253e5acdfbe3ab48492829
Angel Melendez <amangelmelendez@gmail.com>
2024-12-17 19:12:41 -0700

    Built Sanity Studio, and modified the IntroTemplate, to look more like a footer of a page

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,62 +1,28 @@
 'use client'
+
 /**
- * This config is used to set up Sanity Studio that's mounted on the `/pages/studio/[[...index]].tsx` route
+ * This configuration is used to for the Sanity Studio that’s mounted on the `/app/studio/[[...tool]]/page.tsx` route
  */
 
-import { debugSecrets } from '@sanity/preview-url-secret/sanity-plugin-debug-secrets'
-import { visionTool } from '@sanity/vision'
-import {
-  apiVersion,
-  dataset,
-  DRAFT_MODE_ROUTE,
-  projectId,
-} from 'lib/sanity.api'
-import { locate } from 'plugins/locate'
-import { previewDocumentNode } from 'plugins/previewPane'
-import { settingsPlugin, settingsStructure } from 'plugins/settings'
-import { defineConfig } from 'sanity'
-import { presentationTool } from 'sanity/presentation'
-import { structureTool } from 'sanity/structure'
-import { unsplashImageAsset } from 'sanity-plugin-asset-source-unsplash'
-import authorType from 'schemas/author'
-import postType from 'schemas/post'
-import settingsType from 'schemas/settings'
+import {visionTool} from '@sanity/vision'
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
 
-const title =
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_TITLE || 'Next.js Blog with Sanity.io'
+// Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
+import {apiVersion, dataset, projectId} from './sanity/env'
+import {schema} from './sanity/schemaTypes'
+import {structure} from './sanity/structure'
 
 export default defineConfig({
   basePath: '/studio',
   projectId,
   dataset,
-  title,
-  schema: {
-    // If you want more content types, you can add them to this array
-    types: [authorType, postType, settingsType],
-  },
+  // Add and edit the content schema in the './sanity/schemaTypes' folder
+  schema,
   plugins: [
-    structureTool({
-      structure: settingsStructure(settingsType),
-      // `defaultDocumentNode` is responsible for adding a “Preview” tab to the document pane
-      defaultDocumentNode: previewDocumentNode(),
-    }),
-    presentationTool({
-      locate,
-      previewUrl: {
-        draftMode: {
-          enable: DRAFT_MODE_ROUTE,
-        },
-      },
-    }),
-    // Configures the global "new document" button, and document actions, to suit the Settings document singleton
-    settingsPlugin({ type: settingsType.name }),
-    // Add an image asset source for Unsplash
-    unsplashImageAsset(),
-    // The remaining plugins are only loaded in dev mode
-    process.env.NODE_ENV !== 'production' && debugSecrets(),
-    // Vision lets you query your content with GROQ in the studio
+    structureTool({structure}),
+    // Vision is for querying with GROQ from inside the Studio
     // https://www.sanity.io/docs/the-vision-plugin
-    process.env.NODE_ENV !== 'production' &&
-      visionTool({ defaultApiVersion: apiVersion }),
+    visionTool({defaultApiVersion: apiVersion}),
   ],
 })
